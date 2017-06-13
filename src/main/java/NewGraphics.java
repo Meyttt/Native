9b252995764be637dd6436dbb9b9b5ae621e67c3
Meyttt <sabermeyt@gmail.com>
2017-06-13 19:33:38 +0300

    Заглушки графики

--- src/main/java/NewGraphics.java
+++ src/main/java/NewGraphics.java
@@ -3,29 +3,64 @@
  */
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class NewGraphics extends Application {
-
+    @FXML
+    Label devList;
+    @FXML
+    ListView list;
+    @FXML
+    Button send;
+    @FXML
+    Button cancel;
+    @FXML
+    Button apply;
+    @FXML
+    TextField period;
+    @FXML
+    Button exit;
+    @FXML
+    Label update;
+    NewGraphics newGraphics;
+    AnchorPane NewProject;
+    static ObservableList<String> results;
     public static void main(String[] args) {
         launch(args);
     }
 
+    public ListView getList() {
+        return list;
+    }
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(NewGraphics.class.getResource("NewGraphics.fxml"));
-        AnchorPane NewProject = loader.load();
+        NewProject= loader.load();
+        newGraphics=loader.getController();
+        newGraphics.list.setItems(this.results);
+        newGraphics.update.setText("Last update: \r\n"+new Date());
+
         Scene scene = new Scene(NewProject);
         primaryStage.setScene(scene);
         primaryStage.setTitle("ObserverClient");
         primaryStage.show();
+
+
+    }
+    public void launch(ObservableList<String> results){
+        this.results = results;
+        launch(NewGraphics.class);
     }
 }
--- src/main/java/SampleSPPClient.java
+++ src/main/java/SampleSPPClient.java
@@ -1,28 +1,33 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import javax.bluetooth.*;
 import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
-import javax.microedition.io.StreamConnectionNotifier;
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Vector;
 
 public class SampleSPPClient implements DiscoveryListener {
 
     //object used for waiting
-    private static Object lock=new Object();
+    private static Object lock = new Object();
+    private static Object lock2 = new Object();
 
     //vector containing the devices discovered
-    private static Vector vecDevices=new Vector();
+    private static Vector vecDevices = new Vector();
 
-    private static String connectionURL=null;
+    private static String connectionURL = null;
 
-    public static void main(String[] args) throws IOException {
+    public static void main0(String[] args) throws IOException {
 
-        SampleSPPClient client=new SampleSPPClient();
+        SampleSPPClient client = new SampleSPPClient();
 
         //display local device address and name
         LocalDevice localDevice = LocalDevice.getLocalDevice();
-        System.out.println("Address: "+localDevice.getBluetoothAddress());
-        System.out.println("Name: "+localDevice.getFriendlyName());
+        System.out.println("Address: " + localDevice.getBluetoothAddress());
+        System.out.println("Name: " + localDevice.getFriendlyName());
 
         //find devices
         DiscoveryAgent agent = localDevice.getDiscoveryAgent();
@@ -32,11 +37,10 @@ public class SampleSPPClient implements DiscoveryListener {
         agent.startInquiry(DiscoveryAgent.GIAC, client);
 
         try {
-            synchronized(lock){
+            synchronized (lock) {
                 lock.wait();
             }
-        }
-        catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
@@ -44,44 +48,42 @@ public class SampleSPPClient implements DiscoveryListener {
         System.out.println("Device Inquiry Completed. ");
 
         //print all devices in vecDevices
-        int deviceCount=vecDevices.size();
+        int deviceCount = vecDevices.size();
 
-        if(deviceCount <= 0){
+        if (deviceCount <= 0) {
             System.out.println("No Devices Found .");
             System.exit(0);
-        }
-        else{
+        } else {
             //print bluetooth device addresses and names in the format [ No. address (name) ]
             System.out.println("Bluetooth Devices: ");
-            for (int i = 0; i <deviceCount; i++) {
-                RemoteDevice remoteDevice=(RemoteDevice)vecDevices.elementAt(i);
-                System.out.println((i+1)+". "+remoteDevice.getBluetoothAddress()+" ("+remoteDevice.getFriendlyName(true)+")");
+            for (int i = 0; i < deviceCount; i++) {
+                RemoteDevice remoteDevice = (RemoteDevice) vecDevices.elementAt(i);
+                System.out.println((i + 1) + ". " + remoteDevice.getBluetoothAddress() + " (" + remoteDevice.getFriendlyName(true) + ")");
             }
         }
 
         System.out.print("Choose Device index: ");
-        BufferedReader bReader=new BufferedReader(new InputStreamReader(System.in));
-        String chosenIndex=bReader.readLine();
-        int index=Integer.parseInt(chosenIndex.trim());
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+        String chosenIndex = bReader.readLine();
+        int index = Integer.parseInt(chosenIndex.trim());
 
         //check for spp service
-        RemoteDevice remoteDevice=(RemoteDevice)vecDevices.elementAt(index-1);
+        RemoteDevice remoteDevice = (RemoteDevice) vecDevices.elementAt(index - 1);
         UUID[] uuidSet = new UUID[1];
-        uuidSet[0]=new UUID("446118f08b1e11e29e960800200c9a66", false);
+        uuidSet[0] = new UUID("446118f08b1e11e29e960800200c9a66", false);
 
         System.out.println("\nSearching for service...");
-        agent.searchServices(null,uuidSet,remoteDevice,client);
+        agent.searchServices(null, uuidSet, remoteDevice, client);
 
         try {
-            synchronized(lock){
+            synchronized (lock) {
                 lock.wait();
             }
-        }
-        catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        if(connectionURL==null){
+        if (connectionURL == null) {
             System.out.println("Device does not support Simple SPP Service.");
             System.exit(0);
         }
@@ -91,12 +93,12 @@ public class SampleSPPClient implements DiscoveryListener {
 //        dataOutputStream.flush();
 //        dataOutputStream.close();
         //connect to the server and send a line of text
-        StreamConnection streamConnection=(StreamConnection) Connector.open(connectionURL);
+        StreamConnection streamConnection = (StreamConnection) Connector.open(connectionURL);
 
 
         //send string
-        OutputStream outStream=streamConnection.openOutputStream();
-        PrintWriter pWriter=new PrintWriter(new OutputStreamWriter(outStream));
+        OutputStream outStream = streamConnection.openOutputStream();
+        PrintWriter pWriter = new PrintWriter(new OutputStreamWriter(outStream));
         pWriter.write("Test String from SPP Client\r\n");
         pWriter.flush();
         pWriter.close();
@@ -106,12 +108,10 @@ public class SampleSPPClient implements DiscoveryListener {
 //        streamConnection.close();
 
 
-
-
         //read response
-        InputStream inStream=streamConnection.openInputStream();
-        BufferedReader bReader2=new BufferedReader(new InputStreamReader(inStream));
-        String lineRead=bReader2.readLine();
+        InputStream inStream = streamConnection.openInputStream();
+        BufferedReader bReader2 = new BufferedReader(new InputStreamReader(inStream));
+        String lineRead = bReader2.readLine();
         System.out.println(lineRead);
         streamConnection.close();
         /*
@@ -133,34 +133,83 @@ public class SampleSPPClient implements DiscoveryListener {
     //methods of DiscoveryListener
     public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
         //add the device to the vector
-        if(!vecDevices.contains(btDevice)){
+        if (!vecDevices.contains(btDevice)) {
             vecDevices.addElement(btDevice);
         }
     }
 
     //implement this method since services are not being discovered
     public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
-        if(servRecord!=null && servRecord.length>0){
-            connectionURL=servRecord[0].getConnectionURL(0,false);
+        if (servRecord != null && servRecord.length > 0) {
+            connectionURL = servRecord[0].getConnectionURL(0, false);
         }
-        synchronized(lock){
+        synchronized (lock) {
             lock.notify();
         }
     }
 
     //implement this method since services are not being discovered
     public void serviceSearchCompleted(int transID, int respCode) {
-        synchronized(lock){
+        synchronized (lock) {
             lock.notify();
         }
     }
 
 
     public void inquiryCompleted(int discType) {
-        synchronized(lock){
+        synchronized (lock) {
             lock.notify();
         }
 
     }//end method
+
+    public ObservableList<String> getDevicesList() throws IOException {
+        SampleSPPClient client = new SampleSPPClient();
+
+        //display local device address and name
+        LocalDevice localDevice = LocalDevice.getLocalDevice();
+        System.out.println("Address: " + localDevice.getBluetoothAddress());
+        System.out.println("Name: " + localDevice.getFriendlyName());
+
+        //find devices
+        DiscoveryAgent agent = localDevice.getDiscoveryAgent();
+
+        System.out.println("Starting device inquiry...");
+        //todo: почитать про коды(?)
+        agent.startInquiry(DiscoveryAgent.GIAC, client);
+
+        try {
+            synchronized (lock) {
+                lock.wait();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<String> preList = new LinkedList<String>();
+
+        RemoteDevice[] devices = agent.retrieveDevices(0);
+        for (RemoteDevice remoteDevice : devices) {
+            String name = remoteDevice.getFriendlyName(false);
+            if (!(name == null)) {
+                if (!(name.equals(""))) {
+                    preList.add("Name: " + name + "; address: " + remoteDevice.getBluetoothAddress());
+                    continue;
+                }
+            }
+            preList.add("Name:(no name); address: " + remoteDevice.getBluetoothAddress());
+
+        }
+        ObservableList<String> devList = FXCollections.observableArrayList(preList);
+        return devList;
+    }
+
+    public static void main(String[] args) throws IOException {
+        SampleSPPClient sampleSPPClient = new SampleSPPClient();
+        ObservableList<String> devices = sampleSPPClient.getDevicesList();
+        NewGraphics newGraphics = new NewGraphics();
+        newGraphics.launch(devices);
+
+
+    }
 
 }
